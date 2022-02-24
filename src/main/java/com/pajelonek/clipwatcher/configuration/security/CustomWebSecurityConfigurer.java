e05b9d520e7746ef1785fe215588b5c07865b840
pajelonek <pawel.jelonek.krakow@gmail.com>
2022-02-24 17:28:21 +0100

    Allowing access to healtcheck without basic auth

--- src/main/java/com/pajelonek/clipwatcher/configuration/security/CustomWebSecurityConfigurer.java
+++ src/main/java/com/pajelonek/clipwatcher/configuration/security/CustomWebSecurityConfigurer.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -22,6 +24,13 @@ public class CustomWebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Value("${authentication.password}")
     private String password;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers(HttpMethod.GET, "/actuator/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
