299b083ce8c411151306d4a943a36c081445daa0
pajelonek <pawel.jelonek.krakow@gmail.com>
2022-02-25 10:58:25 +0100

    Adding new test to verify sonar coverage

--- src/test/java/com/pajelonek/clipwatcher/ClipWatcherApplicationTest.java
+++ src/test/java/com/pajelonek/clipwatcher/ClipWatcherApplicationTest.java
@@ -1,18 +1,19 @@
 package com.pajelonek.clipwatcher;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ActiveProfiles("LOCAL")
-@SpringBootTest
-class ClipWatcherApplicationTests {
+@RunWith(SpringRunner.class)
+class ClipWatcherApplicationTest {
 
 	@Test
-	void contextLoads() {
+	void applicationStarts() {
+		ClipWatcherApplication.main(new String[]{});
 		assertThat(true).isTrue();
 	}
-
 }
