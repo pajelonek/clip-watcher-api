c473fcf2b355a6c27a3e6440b65237e3ba191bcf
pajelonek <pawel.jelonek.krakow@gmail.com>
2022-03-25 15:37:06 +0100

    Test cors configuration

--- src/main/java/com/pajelonek/clipwatcher/ClipWatcherApplication.java
+++ src/main/java/com/pajelonek/clipwatcher/ClipWatcherApplication.java
@@ -37,7 +37,7 @@ public class ClipWatcherApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/*")
-						.allowedOrigins(corsOrigin);
+						.allowedOrigins("*");
 			}
 		};
 	}
