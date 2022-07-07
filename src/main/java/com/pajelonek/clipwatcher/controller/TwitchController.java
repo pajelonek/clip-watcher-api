69d7a0683cccc0ec34d1b7cd97100444d01bffad
pajelonek <pawel.jelonek.krakow@gmail.com>
2022-07-07 20:39:51 +0200

    Removing cors property

--- src/main/java/com/pajelonek/clipwatcher/controller/TwitchController.java
+++ src/main/java/com/pajelonek/clipwatcher/controller/TwitchController.java
@@ -14,7 +14,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class TwitchController {
 
