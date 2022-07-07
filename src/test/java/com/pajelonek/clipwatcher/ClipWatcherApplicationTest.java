7b1a866b0e949b3bcec9ba6eff7bd619a415dc43
pajelonek <pawel.jelonek.krakow@gmail.com>
2022-07-07 20:29:23 +0200

    Updating pom to 1.0.0 - migration to digital ocean

--- src/test/java/com/pajelonek/clipwatcher/ClipWatcherApplicationTest.java
+++ src/test/java/com/pajelonek/clipwatcher/ClipWatcherApplicationTest.java
@@ -2,12 +2,10 @@ package com.pajelonek.clipwatcher;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@ActiveProfiles("LOCAL")
 @RunWith(SpringRunner.class)
 class ClipWatcherApplicationTest {
 
